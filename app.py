419b12b47044abb76fd6a214bcc8c323671fc6e3
Maxen <corpmaxen@gmail.com>
2025-06-25 11:02:14 +0200

    Delete 0 value

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import os
 import sys
 from werkzeug.utils import secure_filename
-from werkzeug.security import check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash   
 from dotenv import load_dotenv
 import mimetypes
 from config import get_config
